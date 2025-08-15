75f2a35847e24eeb02a88cd5fa1c61f9a4aa1c93
TheRealCrazyfuy <TheRealCrazyfuy@users.noreply.github.com>
2025-08-15 18:28:55 +0200

    Win activation

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     // hide the loader element and show the page content
     loader.style.display = "none";
     content.style.display = "block";
+
+
+    if (isWindows()) {
+      const activateWindows = document.getElementById("activate-windows");
+      activateWindows.style.display = "block";
+    }
   }, 500); // delay it a bit
 });
+
+function isWindows() {
+  return navigator.userAgent.includes("Windows") || navigator.platform.includes("Win");
+}
